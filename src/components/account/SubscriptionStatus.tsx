2c5d144c8bbef37f7683b367ddf801398d27737e
drhemanm <heman.m@iafrica.solutions>
2025-09-11 13:25:39 +0400

    Update SubscriptionStatus.tsx

--- src/components/account/SubscriptionStatus.tsx
+++ src/components/account/SubscriptionStatus.tsx
@@ -37,7 +37,7 @@ export function SubscriptionStatus() {
 
   if (plan === 'free') {
     const daysLeft = subscription?.trialEndDate 
-      ? Math.ceil((subscription.trialEndDate.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24))
+      ? Math.ceil((new Date(subscription.trialEndDate).getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24))
       : 30;
 
     return (
