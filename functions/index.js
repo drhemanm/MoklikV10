fcfd15523f2fe78d496d3f2f89700840a2c0dc64
drhemanm <heman.m@iafrica.solutions>
2025-09-12 10:46:46 +0400

    Create index.js

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.paypalWebhook = functions.https.onRequest(async (req, res) => {
+  console.log('PayPal webhook received:', req.body);
+  
+  // Handle PayPal subscription events here
+  const event = req.body;
+  
+  if (event.event_type === 'BILLING.SUBSCRIPTION.PAYMENT.COMPLETED') {
+    console.log('Payment completed for subscription:', event.resource.id);
+  }
+  
+  res.status(200).send('OK');
+});
